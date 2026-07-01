53429efc0adcbb0588245e015871dcd87fb568fe
prisshagala-ui <prissha.gala@jmlschool.org>
2026-07-01 20:26:24 +0530

    intro to python

---
+++ main.py
@@ -0,0 +1,10 @@
+#printing the greeting message
+print("hey python")
+
+#printing my name
+print("my name is Prissha")
+
+#priting value
+print(6)
+
+print("the result of 3+5 is ",3+5 ,"is the answer")
